161b468b3b39c7d733169056b580b9340e437eee
wernier <wernierschultz@yahoo.com.br>
2023-05-17 21:02:30 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,12 +23,15 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-	       }
+			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
@@ -37,6 +40,6 @@ public class Program {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-	 }	
-   }
+		}
+	}
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,8 +17,8 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
-	protected boolean isThereOpponentePiece(Position position) {
+	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
-		return p != null && p.getColor() != color;		
+		return p != null && p.getColor() != color;
 	}
 }
